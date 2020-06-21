1482b29b06907d726ed9dce7fc8a5eb002789238
Agustin Martinez <agustin.martinez@rootstrap.com>
2020-06-21 18:54:07 -0300

    Initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,23 @@
+import React from "react"
+import { Link } from "gatsby"
+
+
+export default function HomeMod() {
+  return (
+    <div style={{ color: `purple` }}>
+      <h1>Hello Gatsby!</h1>
+      <p>What a world.</p>
+      <Link to='/about'>About</Link>
+    </div>
+  );
+}
+
+export function Home() {
+  return (
+    <div style={{ color: `purple` }}>
+      <h1>Hello Gatsby!</h1>
+      <p>What a world.</p>
+      <img src="https://source.unsplash.com/random/400x200" alt="" />
+    </div>
+  )
+}
