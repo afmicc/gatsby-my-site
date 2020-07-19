68aa784431c7c0363a533c55d768a54cb48efd99
Agustin Martinez <agustin.martinez@rootstrap.com>
2020-07-19 20:55:32 -0300

    Inverse cards backgtound colors

--- src/components/Section/card.js
+++ src/components/Section/card.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
-const Card = ({title, content, children, color }) => (
-  <div className="card d-flex fd-column">
+const Card = ({title, content, children, color, background }) => (
+  <div className={ `card ${background ? 'card--background-default' : 'card--background-inverse' } d-flex fd-column` }>
     <h2 className={ `card__title m-0 card__title--${gerColor(color)}` }>{title}</h2>
     <p className="card__text m-0">
       {children || content}
